0d45aa4dd29783274735982cf8f2e56508de3c48
lim jiho <winterziho@gmail.com>
2022-04-04 17:58:29 +0900

    대문 페이지와 자기소개 페이지 만들기

--- single_pages/urls.py
+++ single_pages/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .import views
+from . import views
 
 urlpatterns = [
     path('about_me/', views.about_me),
-    path('',views.landing),
+    path('', views.landing),
 ]
